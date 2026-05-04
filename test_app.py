c1651c6cc8bf09999eeafd8668da5a61077534b0
bandarichandra510 <bandarichandra510@gmail.com>
2026-05-05 00:41:33 +0530

    Create test_app.py
    test_app.py commit1

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import greet
+
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("Alice"), "Hello, Alice!")
+
+if __name__ == "__main__":
+    unittest.main()
